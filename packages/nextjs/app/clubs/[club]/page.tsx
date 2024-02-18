ecd6c2a657afde483625c4558e4df0bf5e60a3d3
egidioquad <egidioquadrini97@gmail.com>
2024-02-18 08:29:10 +0100

    almost done

--- packages/nextjs/app/clubs/[club]/page.tsx
+++ packages/nextjs/app/clubs/[club]/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { Box, Button } from "grommet";
+import { Box, Button, Grid, Image, Meter, ResponsiveContext } from "grommet";
 import type { NextPage } from "next";
 import { InputBase } from "~~/components/scaffold-eth";
 import deployedContracts from "~~/contracts/deployedContracts";
 import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { GetFileFromIpfs } from "~~/utilComponents/IPFSdeploy";
+import { ExtendedCampaign } from "~~/utilComponents/campaignInterface";
 
 interface Campaign {
   creator: string;
@@ -20,7 +22,7 @@ interface Campaign {
 
 interface CampaignBoxProps {
   index: number;
-  campaign: Campaign;
+  campaign: ExtendedCampaign;
 }
 
 const fundRaisingAddress = deployedContracts[421614].Fundraising.address;
@@ -48,13 +50,15 @@ const CampaignBox: React.FC<CampaignBoxProps> = ({ index, campaign }) => {
   });
 
   return (
-    <Box border={{ color: "brand", size: "small" }} pad="medium" margin={{ bottom: "medium" }}>
+    /*  <Box border={{ color: "brand", size: "small" }} pad="medium" margin={{ bottom: "medium" }}>
       <h1 className="text-lg">Creator: {campaign.creator}</h1>
       <h1 className="text-lg">IPFS: {campaign.ipfs}</h1>
       <h1 className="text-lg">Club: {campaign.club}</h1>
       <h1 className="text-lg">Goal Amount: {campaign.goalAmount.toString()}</h1>
       <h1 className="text-lg">Current Amount: {campaign.currentAmount.toString()}</h1>
       <h1 className="text-lg">Finalized: {campaign.finalized ? "Yes" : "No"}</h1>
+      <h1 className="text-lg">Title: {campaign.title}</h1>
+      <h1 className="text-lg">Description: {campaign.description}</h1>
       <h1 className="text-lg">End Campaign: {campaign.endCampaign.toString()}</h1>
 
       {!campaign.finalized && (
@@ -72,33 +76,250 @@ const CampaignBox: React.FC<CampaignBoxProps> = ({ index, campaign }) => {
       )}
 
       {result && <p>{result}</p>}
+    </Box> */
+
+    <Box pad="large" margin={{ horizontal: "medium" }}>
+      <Box direction="row" background="#D9D9D9" justify="between" round="medium" width="900px">
+        <Box
+          align="start"
+          justify="start"
+          gap="medium"
+          margin={{ horizontal: "medium", right: "large", vertical: "medium", top: "small" }}
+        >
+          <Box>
+            <h1 className="text-xl text-black">About the project</h1>
+          </Box>
+          <Box>
+            <h1 className="text-black">{campaign.description}</h1>
+          </Box>
+        </Box>
+        <Box align="end" justify="center" margin={{ top: "medium" }}>
+          <Box
+            background="white"
+            width={{ max: "medium" }}
+            border={{ color: "#a3e635", size: "medium" }}
+            round="medium"
+            pad="small"
+            margin={{ vertical: "medium", right: "large" }}
+          >
+            <Box align="center" pad="small" margin={{ top: "medium" }}>
+              <h1>{campaign.title}</h1>
+            </Box>
+            <Box pad={{ bottom: "medium", horizontal: "medium" }}>
+              <Meter
+                background="#cbd5e1"
+                color="#a3e635"
+                type="bar"
+                value={Number(campaign.currentAmount)}
+                max={Number(campaign.goalAmount)}
+              />
+              <Box direction="row" justify="between" pad="small" margin={{ top: "medium" }}>
+                <Box>
+                  <h1>Raised:</h1>
+                  <h1>${campaign.currentAmount.toString()}</h1>
+                </Box>
+                <Box>
+                  <h1>Goal:</h1>
+                  <h1>${campaign.goalAmount.toString()}</h1>
+                </Box>
+              </Box>
+            </Box>
+            {!campaign.finalized && (
+              <>
+                <Box pad="small" width={{ max: "70%" }} align="center">
+                  <InputBase name="Enter amount" value={amount} onChange={setAmount} />
+                </Box>
+
+                <Button
+                  primary
+                  color="#a3e635"
+                  label="DONATE"
+                  onClick={async () => {
+                    await approve();
+                    await contributeFr();
+                    setResult(`Function executed with amount: ${amount}`);
+                  }}
+                />
+              </>
+            )}
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
 
 const ClubPage: NextPage = () => {
   const pathParts = usePathname().split("/");
-  const id = pathParts[pathParts.length - 1];
+  const club = pathParts[pathParts.length - 1];
 
   const { data: filteredCampaigns } = useScaffoldContractRead({
     contractName: "Fundraising",
     functionName: "getAllCampaignsByClub",
-    args: [id],
+    args: [club],
   });
 
+  const [campaignsByClub, setCampaignsByClub] = useState<ExtendedCampaign[]>([]);
+
+  useEffect(() => {
+    const handleIPFS = async (filteredCampaigns: Campaign[]) => {
+      console.log("ciao");
+      const allCampaigns: ExtendedCampaign[] = [];
+      for (const campaign of filteredCampaigns) {
+        if (campaign.ipfs) {
+          try {
+            const ipfsData = await GetFileFromIpfs(campaign.ipfs);
+            const extendedCampaign: ExtendedCampaign = {
+              ...campaign,
+              description: ipfsData.description,
+              title: ipfsData.title,
+            };
+            allCampaigns.push(extendedCampaign);
+          } catch (error) {
+            console.error(`Error fetching data for campaign with IPFS hash ${campaign.ipfs}:`, error);
+          }
+        }
+      }
+      console.log("allcampaigns with title", allCampaigns);
+      setCampaignsByClub(allCampaigns);
+    };
+
+    if (filteredCampaigns) {
+      const campaignsArray: Campaign[] = filteredCampaigns as Campaign[];
+      handleIPFS(campaignsArray);
+    }
+  }, [filteredCampaigns]);
+
+  const size = React.useContext(ResponsiveContext);
+
   return (
-    <Box margin="large">
-      <h1 className="text-2xl">{id}</h1>
-      <h1 className="text-xl">Projects</h1>
-      {filteredCampaigns && filteredCampaigns.length > 0 ? (
-        filteredCampaigns.map((campaign: Campaign, index: number) => (
-          <CampaignBox key={index} index={index} campaign={campaign} />
-        ))
-      ) : (
-        <h1>No campaigns yet.</h1>
-      )}
+    <Box>
+      <Box width="full" height="auto" background="#a3e635" direction="row" align="start">
+        <Box pad="medium">
+          <Box>
+            <Image src={`/42Hack/${club}.png`} fit="contain" width="30%" className="rounded-full" />
+          </Box>
+        </Box>
+        <Box width={{ max: "50%" }} justify="center" pad="medium">
+          {/*  */}
+          <h1 className="text-xl text-black ">About {club}</h1>
+          {club === "42book" && (
+            <Box gap="small">
+              <h1 className=" text-black leading-2">
+                The club organizes events such as reading sessions, meetings with authors, visits to local bookstores,
+                and book exchanges. A great place to explore new titles and connect with other reading lovers.{" "}
+              </h1>
+            </Box>
+          )}
+          {club === "42activities" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                The club offers students a variety of exciting experiences outside the classroom. We organize field
+                trips, museum visits, sports activities, special events and happy hours to promote physical and mental
+                well-being. Join us to create unforgettable memories.
+              </h1>
+            </Box>
+          )}
+          {club === "42spaghetti" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                The ideal place for aspiring game developers. In this club, students will have the opportunity to
+                cultivate their skills and carry out creative projects. Come out to us to explore the world of the video
+                game industry and turn your ideas into interactive experiences.
+              </h1>
+            </Box>
+          )}
+          {club === "42startup" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club is an incubator of innovative ideas and a meeting place for aspiring entrepreneurs. We offer
+                support, networking and resources to help startups grow and thrive in the entrepreneurial ecosystem.
+              </h1>
+            </Box>
+          )}
+          {club === "42cybersecurity" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club is a meeting place for enthusiasts and professionals eager to learn more about knowledge and
+                practices in the field of cybersecurity. We offer workshops, conferences and resources to address the
+                challenges of data protection in the digital world. We also participate in national competitions in the
+                field.
+              </h1>
+            </Box>
+          )}
+          {club === "42music" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club is the perfect place to explore, create and share the magic of music. We offer jam sessions,
+                live events, workshops, performance spaces and collaborations for enthusiasts and musicians of all
+                levels.
+              </h1>
+            </Box>
+          )}
+          {club === "42pingpong" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                the club is a gathering place for lovers of this exciting sport. We offer game sessions, friendly
+                tournaments and coaching to promote passion and skill in pin pong, creating a fun and inclusive
+                environment for all levels of players.
+              </h1>
+            </Box>
+          )}
+          {club === "42ciak" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club is the ideal haven for film enthusiasts, offering screenings, discussions and special events to
+                explore the world of filmmaking. Join us to discover masterpieces, share opinions, and cultivate your
+                passion for film.
+              </h1>
+            </Box>
+          )}
+          {club === "42freelance" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club is a hub of resources and connections for independent professionals seeking opportunities and
+                support. We hold networking events, share resources and offer consultations to help freelancers improve
+                in their career path.
+              </h1>
+            </Box>
+          )}
+          {club === "42gaming" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club is the perfect place for gaming enthusiasts, offering tornerà, social gaming sessions and
+                special events to connect players. Join us for unforgettable gaming experiences and create lasting
+                community connections.
+              </h1>
+            </Box>
+          )}
+          {club === "42BDE" && (
+            <Box gap="small">
+              <h1 className=" text-black  leading-2">
+                Our club offers unique experiences through a wide range of indoor and outdoor events. With creative
+                organization and dedication, we create unforgettable moments that unite people, inspire and celebrate
+                the diversity of our community.
+              </h1>
+            </Box>
+          )}
+          {/*  */}
+        </Box>
+      </Box>
+
+      <Grid columns={size === "large" ? ["1/2", "1/2"] : "large"}>
+        {campaignsByClub && campaignsByClub.length > 0 ? (
+          campaignsByClub.map((campaign: ExtendedCampaign, index: number) => (
+            <CampaignBox key={index} index={index} campaign={campaign} />
+          ))
+        ) : (
+          <h1>No campaigns yet.</h1>
+        )}
+      </Grid>
     </Box>
   );
 };
 
 export default ClubPage;
+
+interface ClubCardProps {
+  campaign: ExtendedCampaign;
+}
--- packages/nextjs/utilComponents/landingPage.tsx
+++ packages/nextjs/utilComponents/landingPage.tsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
       <Grid columns={["2/4", "1/3"]} margin="medium" gap="medium">
         <Box width={{ max: "xlarge" }} gap="medium" margin={{ top: "medium" }}>
           <h1 className="text-3xl font-bold text-black ">Empowering greatness, made easy.</h1>
-          <h1 className="text-black leading-relaxed max-w-xl">
+          <h1 className="text-black leading-relaxed max-w-2xl">
             Support your community projects! Invest now and receive 42 tokens to access exclusive campus benefits. Get
             involved and make a difference today!
           </h1>
